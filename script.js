b9f9b6c2a4354bf8078fb18bc1f99c0899d0231a
Debugger12116 <grinevicius.gustas@gmail.com>
2025-12-17 21:28:32 +0200

    pakeistas alertas del issiusto laisko

--- script.js
+++ script.js
@@ -578,7 +578,7 @@ async function resetPassword() {
 
     try {
         await sendPasswordResetEmail(auth, email);
-        alert(`Slaptažodžio atkūrimo laiškas išsiųstas į ${email}. Patikrinkite paštą (ir Spam aplanką)!`);
+        alert(`Slaptažodžio atkūrimo laiškas išsiųstas į ${email}.`);
     } catch (error) {
         console.error(error);
         let msg = "Klaida siunčiant laišką.";
